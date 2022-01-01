b433aa42cba4aa0cb3ea9e2270ac4928dc959801
Pedro <pedrodavila.bovolin@gmail.com>
2022-01-01 04:45:58 -0300

    Hero section finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,20 @@ const openNav = () => {
 }
 
 mobileMenu.addEventListener('click', openNav);
+
+// Hero Animations
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.hero-animate', {
+    duration: 0.8,
+    opacity: 0,
+    x: -150,
+    stagger: 0.1
+})
+
+gsap.from('.hero-animate-image', {
+    duration: 1,
+    opacity: 0,
+    x: -150,
+})
