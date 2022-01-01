56980327830985a96f00616243328583a0c6b922
Pedro <pedrodavila.bovolin@gmail.com>
2022-01-01 08:19:32 -0300

    Servicos finalizados

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,19 @@ gsap.from('.hero-animate-image', {
     opacity: 0,
     x: -150,
 })
+
+gsap.from('.about-heading-animate', {
+    scrollTrigger: '.about',
+    duration: 0.8,
+    opacity: 0,
+    y: -150,
+    stagger: 0.4
+})
+
+gsap.from('.about-card-animate', {
+    scrollTrigger: '.about',
+    duration: 1,
+    opacity: 0,
+    x: -150,
+    stagger: 0.5
+})
